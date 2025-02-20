12b9cb975253fde2c4b8e11255ef3a2826fbe42a
ananthram-dotcom <ananthramsuresh05@gmail.com>
2025-02-20 15:22:58 +0530

    Day 20/02/2024

---
+++ web/script.js
@@ -0,0 +1,25 @@
+let a = "Ananth Ram" //string
+a = 10 //number
+a = false //boolean
+console.console.log(a);
+const b = "Ananth Ram"
+//b=10
+console.log(b)
+//array
+let array1=[a,b,true,10,"hello"]
+console.log(array1)
+console.log(array1[1])
+//js object
+let jsobj={
+    name: "a",
+    age: 12,
+    isalive: true
+}
+//json object
+let jsonobj={
+    "name": "a",
+    "age": 12,
+    "isalive": true
+}
+console.log(jsonobj)
+console.log(jsonobj)
